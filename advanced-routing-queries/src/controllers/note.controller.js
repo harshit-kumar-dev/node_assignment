f7dc755f488abdcf370ba2e216f66ca76a4940f9
Harshit kumar <harshit.kumar.cg@gmail.com>
2026-03-01 10:00:00 +0530

    chore: initial project setup for advanced routing queries

---
+++ advanced-routing-queries/src/controllers/note.controller.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const Note = require("../models/note.model");
+
+// Controller methods will go here
+
+module.exports = {};
---
+++ advanced-routing-queries/src/routes/note.routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const noteController = require("../controllers/note.controller");
+
+// Route mappings will go here
+
+module.exports = router;
