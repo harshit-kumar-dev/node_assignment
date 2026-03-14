09646e38a904eeac4af4f5ea6af41ee835979ae0
Harshit kumar <harshit.kumar.cg@gmail.com>
2026-03-14 10:00:00 +0530

    feat: add bulk delete notes endpoint (DELETE /api/notes/bulk)

--- advanced-routing-queries/src/routes/note.routes.js
+++ advanced-routing-queries/src/routes/note.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const noteController = require("../controllers/note.controller");
 
 router.post("/bulk", noteController.createBulkNotes);
+router.delete("/bulk", noteController.deleteBulkNotes);
 router.post("/", noteController.createNote);
 router.get("/", noteController.getAllNotes);
 router.get("/:id", noteController.getNoteById);
