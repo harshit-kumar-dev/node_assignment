bee7433127f49c4ad1da64b335d25ad0ee0c3a3a
Harshit kumar <harshit.kumar.cg@gmail.com>
2026-03-15 10:00:00 +0530

    feat: add get notes by category endpoint (GET /api/notes/category/:category)

--- advanced-routing-queries/src/routes/note.routes.js
+++ advanced-routing-queries/src/routes/note.routes.js
@@ -4,6 +4,7 @@ const noteController = require("../controllers/note.controller");
 
 router.post("/bulk", noteController.createBulkNotes);
 router.delete("/bulk", noteController.deleteBulkNotes);
+router.get("/category/:category", noteController.getNotesByCategory);
 router.post("/", noteController.createNote);
 router.get("/", noteController.getAllNotes);
 router.get("/:id", noteController.getNoteById);
