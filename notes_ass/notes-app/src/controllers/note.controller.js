d92092d6374a934ae8855988bdc2ea319ab67461
Harshit kumar <harshit.kumar.cg@gmail.com>
2026-04-12 10:00:00 +0530

    chore: initial project setup

---
+++ notes_ass/notes-app/src/controllers/note.controller.js
@@ -0,0 +1,5 @@
+const Note = require("../models/note.model");
+
+// Controllers will be added here
+
+module.exports = {};
---
+++ notes_ass/notes-app/src/routes/note.routes.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const noteController = require("../controllers/note.controller");
+
+// Routes will be added here
+
+module.exports = router;
